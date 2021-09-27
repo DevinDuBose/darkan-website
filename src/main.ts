5132b821631df692e1274b3089cbaf4775508f21
Devin DuBose <devin.a.dubose@gmail.com>
2021-09-26 22:36:08 -0700

    Split apart partials & global html/css so that it can be re-used. Add watch-css script for compiling scss. Git ignore compiled CSS.

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const hbs = require('hbs');
 
+  app.setViewEngine('hbs');
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', 'views/partials'));
+
   await app.listen(3000);
 }
+
 bootstrap();
